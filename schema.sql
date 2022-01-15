d0f253a5b1bc210e596a254b500225d8444569c7
thankarezos <thankarezos@gmail.com>
2022-01-15 18:34:45 +0200

    pieces

--- schema.sql
+++ schema.sql
@@ -7,11 +7,43 @@ BEGIN
         `x` TINYINT(1) NOT NULL,
         `y` TINYINT(1) NOT NULL,
         `pieceID` int(2),
-        primary key (`x`,`y`)
+        primary key (`x`,`y`),
+        FOREIGN KEY (pieceID) REFERENCES pieces(pieceID)
     );
 
     INSERT INTO `Board` VALUES (1,1,NULL),(1,2,NULL),(1,3,NULL),(1,4,NULL),(2,1,NULL),(2,2,NULL),(2,3,NULL),(2,4,NULL),(3,1,NULL),(3,2,NULL),(3,3,NULL),(3,4,NULL),(4,1,NULL),(4,2,NULL),(4,3,NULL),(4,4,NULL);
 END ;;
+
+DROP TABLE IF EXISTS pieces;
+CREATE TABLE `pieces`(
+    `pieceID` INT(2) AUTO_INCREMENT,
+    `piececolor` enum('black','white')not null,
+    `shape` enum('cycle','square')not null,
+    `size` enum('long','short')not null,
+    `hole` enum('YES','NO'),
+    `available` enum('TRUE','FALSE') default 'TRUE',
+    primary key (`pieceID`)
+);
+
+INSERT INTO `pieces` (piececolor,shape,size,hole)
+VALUES 
+('black','cycle','long','YES'),
+('black','cycle','long','NO'),
+('black','cycle','short','YES'),
+('black','cycle','short','NO'),
+('black','square','long','YES'),
+('black','square','long','NO'),
+('black','square','short','YES'),
+('black','square','short','NO'),
+
+('white','cycle','long','YES'),
+('white','cycle','long','NO'),
+('white','cycle','short','YES'),
+('white','cycle','short','NO'),
+('white','square','long','YES'),
+('white','square','long','NO'),
+('white','square','short','YES'),
+('white','square','short','NO');
 
 
 DROP TABLE IF EXISTS `game_status`;
