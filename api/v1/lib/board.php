fe25a9fe19b183b047c6cb8fe5d060feeed3815d
Periklis-Chr <it175073@it.teithe.gr>
2022-01-18 21:39:16 +0200

    den mpo0ro allo

--- api/v1/lib/board.php
+++ api/v1/lib/board.php
@@ -32,3 +32,41 @@ function piece_list()
     $res = $st->get_result();
     print json_encode($res->fetch_all(), JSON_PRETTY_PRINT);
 }
+
+
+function chech_valid($token){
+
+    global $mysqli;
+
+    $sql = 'SELECT player as "pl" FROM players where token=?';
+    $st = $mysqli->prepare($sql);
+    $st->bind_param('s', $token );
+    $st->execute();
+    $res = $st->get_result();
+    $x = $res->fetch_assoc();
+    $x['pl'];
+    echo $x['pl'];
+
+    $sql1 = 'SELECT turn ass "tr" FROM game_status';
+    $st1 = $mysqli->prepare($sql1);
+    $st1->bind_param('s', $token );
+    $st1->execute();
+    $res1 = $st1->get_result();
+    $y = $res1->fetch_assoc();
+    $y['tr'];
+    echo $y['tr'];
+
+    $sql2 =  'SELECT state FROM game_status ORDER BY id DESC LIMIT 1';
+    $st2 = $mysqli->prepare($sql2);
+    $st2->execute();
+    $res2 = $st2->get_result();
+    $z = $res2->fetch_assoc();
+
+    if($x['pl'] == $y['tr']){
+        
+    }
+}
+
+
+
+function 
